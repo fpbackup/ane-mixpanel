1d3cf9f1b1708437500c14bcec14802b7f175a37
Matyas Forian Szabo <idontgive@myemail.com>
2014-11-25 15:04:19 +0000

    Add MixPanel iOS library

---
+++ mixpanel-ios/target/checkout/src/xcode/build/mixpanel-ios.build/Release-iphonesimulator/mixpanel-ios.build/Objects-normal/i386/PaymentUtils.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/hudejo/CODE/ane-mixpanel/mixpanel-ios/target/checkout/src/xcode/mixpanel-ios/PaymentUtils.m \
+  /Users/hudejo/CODE/ane-mixpanel/mixpanel-ios/target/checkout/src/xcode/mixpanel-ios/PaymentUtils.h \
+  /Users/hudejo/CODE/ane-mixpanel/mixpanel-ios/target/checkout/src/xcode/mixpanel-ios/FlashRuntimeExtensions.h
---
+++ mixpanel-ios/target/checkout/src/xcode/build/mixpanel-ios.build/Release-iphonesimulator/mixpanel-ios.build/Objects-normal/x86_64/InAppPurchase_iOS.d
@@ -0,0 +1,5 @@
+dependencies: \
+  /Users/hudejo/CODE/ane-mixpanel/mixpanel-ios/target/checkout/src/xcode/mixpanel-ios/InAppPurchase_iOS.m \
+  /Users/hudejo/CODE/ane-mixpanel/mixpanel-ios/target/checkout/src/xcode/mixpanel-ios/InAppPurchase_iOS.h \
+  /Users/hudejo/CODE/ane-mixpanel/mixpanel-ios/target/checkout/src/xcode/mixpanel-ios/FlashRuntimeExtensions.h \
+  /Users/hudejo/CODE/ane-mixpanel/mixpanel-ios/target/checkout/src/xcode/mixpanel-ios/PaymentUtils.h
