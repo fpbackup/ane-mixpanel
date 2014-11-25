645467a85c7432960724b7ddf5d9da62ea886345
Matyas Forian Szabo <idontgive@myemail.com>
2014-11-25 17:13:04 +0000

    remove build files, add example project .iml files

--- mixpanel-ios/target/checkout/src/xcode/build/mixpanel-ios.build/Debug-iphoneos/mixpanel-ios.build/Objects-normal/armv7/PaymentUtils.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/hudejo/CODE/ane-mixpanel/mixpanel-ios/target/checkout/src/xcode/mixpanel-ios/PaymentUtils.m \
-  /Users/hudejo/CODE/ane-mixpanel/mixpanel-ios/target/checkout/src/xcode/mixpanel-ios/PaymentUtils.h \
-  /Users/hudejo/CODE/ane-mixpanel/mixpanel-ios/target/checkout/src/xcode/mixpanel-ios/FlashRuntimeExtensions.h
